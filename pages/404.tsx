4949634db7fcfaabb97fcc6d9e0ac4c1694a4384
dzucconi <mail@damonzucconi.com>
2022-02-08 22:25:14 -0500

    Adds error pages

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import { Box, Button, Cell, Input, Stack } from "@auspices/eos";
+import { DefinitionList } from "../components/core/DefinitionList";
+import { NavigationLayout } from "../components/layouts/NavigationLayout";
+import { Meta } from "../components/core/Meta";
+
+const Error404Page = () => {
+  return (
+    <>
+      <Meta title="Not Found" />
+
+      <DefinitionList
+        definitions={[
+          { term: "Status", definition: "404" },
+          { term: "Error", definition: "Not Found" },
+          {
+            term: "Description",
+            definition:
+              "The requested resource could not be found but may be available in the future. Subsequent requests by the client are permissible.",
+          },
+        ]}
+      />
+    </>
+  );
+};
+
+Error404Page.getLayout = NavigationLayout;
+
+export default Error404Page;
---
+++ pages/500.tsx
@@ -0,0 +1,28 @@
+import { Box, Button, Cell, Input, Stack } from "@auspices/eos";
+import { DefinitionList } from "../components/core/DefinitionList";
+import { NavigationLayout } from "../components/layouts/NavigationLayout";
+import { Meta } from "../components/core/Meta";
+
+const Error500Page = () => {
+  return (
+    <>
+      <Meta title="Internal Server Error" />
+
+      <DefinitionList
+        definitions={[
+          { term: "Status", definition: "500" },
+          { term: "Error", definition: "Internal Server Error" },
+          {
+            term: "Description",
+            definition:
+              "A generic error message, given when an unexpected condition was encountered and no more specific message is suitable.",
+          },
+        ]}
+      />
+    </>
+  );
+};
+
+Error500Page.getLayout = NavigationLayout;
+
+export default Error500Page;
