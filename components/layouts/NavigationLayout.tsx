98343e8b093eb6c7110c87cd47a6852be893ecc8
dzucconi <mail@damonzucconi.com>
2023-02-16 10:29:25 -0500

    Adds temporary banner

--- components/layouts/NavigationLayout.tsx
+++ components/layouts/NavigationLayout.tsx
@@ -1,14 +1,27 @@
-import { Stack } from "@auspices/eos";
+import { Banner, Box, Stack } from "@auspices/eos";
 import { ReactElement } from "react";
 import { Page } from "../core/Page";
 import { Navigation } from "../pages/Navigation";
 
 export const NavigationLayout = (page: ReactElement) => (
-  <Page>
-    <Stack spacing={6}>
-      <Navigation />
+  <>
+    <Banner bg="external">
+      <Box
+        color="primary"
+        as="a"
+        href="https://jttnyc.com/exhibitions/marlon-mullen/when-you-re-here-you-re-familiar"
+        target="_blank"
+      >
+        When You’re Here, You’re Familiar — through April 1 @ JTT
+      </Box>
+    </Banner>
 
-      {page}
-    </Stack>
-  </Page>
+    <Page>
+      <Stack spacing={6}>
+        <Navigation />
+
+        {page}
+      </Stack>
+    </Page>
+  </>
 );
