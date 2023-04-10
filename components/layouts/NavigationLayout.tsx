aeb4b6225f1f80bbdea5ff70afb07c71406f78c5
dzucconi <mail@damonzucconi.com>
2023-04-10 16:39:42 -0400

    Removes banner

--- components/layouts/NavigationLayout.tsx
+++ components/layouts/NavigationLayout.tsx
@@ -1,27 +1,14 @@
-import { Banner, Box, Stack } from "@auspices/eos";
+import { Stack } from "@auspices/eos";
 import { ReactElement } from "react";
 import { Page } from "../core/Page";
 import { Navigation } from "../pages/Navigation";
 
 export const NavigationLayout = (page: ReactElement) => (
-  <>
-    <Banner bg="external">
-      <Box
-        color="currentColor"
-        as="a"
-        href="https://jttnyc.com"
-        target="_blank"
-      >
-        When You’re Here, You’re Familiar — through April 1 @ JTT
-      </Box>
-    </Banner>
+  <Page>
+    <Stack spacing={6}>
+      <Navigation />
 
-    <Page>
-      <Stack spacing={6}>
-        <Navigation />
-
-        {page}
-      </Stack>
-    </Page>
-  </>
+      {page}
+    </Stack>
+  </Page>
 );
