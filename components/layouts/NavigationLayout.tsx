01ef71bf08c39b4dca72b13fed779d39e2d1998d
dzucconi <mail@damonzucconi.com>
2023-11-16 18:49:13 -0500

    Removes banner

--- components/layouts/NavigationLayout.tsx
+++ components/layouts/NavigationLayout.tsx
@@ -1,27 +1,14 @@
-import { Banner, Box, Stack } from "@auspices/eos";
+import { Stack } from "@auspices/eos";
 import { ReactElement } from "react";
 import { Page } from "../core/Page";
 import { Navigation } from "../pages/Navigation";
 
 export const NavigationLayout = (page: ReactElement) => (
-  <>
-    <Banner bg="external">
-      <Box
-        color="currentColor"
-        as="a"
-        href="https://www.spazioveda.it/"
-        target="_blank"
-      >
-        Self-Titled — through November 11 @ Veda
-      </Box>
-    </Banner>
+  <Page>
+    <Stack spacing={6}>
+      <Navigation />
 
-    <Page>
-      <Stack spacing={6}>
-        <Navigation />
-
-        {page}
-      </Stack>
-    </Page>
-  </>
+      {page}
+    </Stack>
+  </Page>
 );
