4a61f34c1e7095f1d43ab0fa22a3b33fe1ec28de
dzucconi <mail@damonzucconi.com>
2025-04-06 22:43:18 -0400

    Adds glosses page

---
+++ pages/glosses.tsx
@@ -0,0 +1,61 @@
+import { Box, Stack } from "@auspices/eos";
+import { gql } from "urql";
+import { prettifyUrl } from "../lib/prettifyUrl";
+import { NavigationLayout } from "../components/layouts/NavigationLayout";
+import { Loading } from "../components/core/Loading";
+import { Meta } from "../components/core/Meta";
+import { buildGetStaticProps, withUrql } from "../lib/urql";
+import { useGlossesQuery } from "../generated/graphql";
+
+const GLOSSES_QUERY = gql`
+  query GlossesQuery {
+    artworks(state: [SELECTED, PUBLISHED]) {
+      id
+      gloss
+      slug
+    }
+  }
+`;
+
+const GlossesPage = () => {
+  const [{ fetching, data, error }] = useGlossesQuery();
+
+  if (error) {
+    throw error;
+  }
+
+  if (fetching || !data) {
+    return <Loading />;
+  }
+
+  const artworks = data.artworks.filter((artwork) => artwork.gloss);
+
+  return (
+    <>
+      <Meta title="Only Websites" />
+
+      <Stack spacing={6}>
+        {artworks.map((artwork) => (
+          <Box
+            key={artwork.slug}
+            as="a"
+            href={`/artworks/${artwork.slug}`}
+            target="_blank"
+            color="external"
+            textAlign="center"
+            // @ts-ignore
+            style={{ textWrap: "balance" }}
+          >
+            {artwork.gloss}
+          </Box>
+        ))}
+      </Stack>
+    </>
+  );
+};
+
+GlossesPage.getLayout = NavigationLayout;
+
+export default withUrql(GlossesPage);
+
+export const getStaticProps = buildGetStaticProps(() => [GLOSSES_QUERY]);
