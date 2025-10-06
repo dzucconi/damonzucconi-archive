657fa4d23486113d058ffd47384c8a1773c49978
dzucconi <mail@damonzucconi.com>
2025-10-06 09:58:51 -0400

    Removes banner for Neurofiction

--- components/layouts/NavigationLayout.tsx
+++ components/layouts/NavigationLayout.tsx
@@ -1,27 +1,14 @@
-import { Banner, Box, Stack } from "@auspices/eos";
+import { Stack } from "@auspices/eos";
 import { ReactElement } from "react";
 import { Page } from "../core/Page";
 import { Navigation } from "../pages/Navigation";
 
 export const NavigationLayout = (page: ReactElement) => (
-  <>
-    <Banner bg="external" dismissable={false}>
-      <Box
-        color="currentColor"
-        as="a"
-        href="https://www.variousartistsrecords.com/zucconi"
-        target="_blank"
-      >
-        Neurofiction — through October 5 @ Various/Artists
-      </Box>
-    </Banner>
+  <Page>
+    <Stack spacing={6}>
+      <Navigation />
 
-    <Page>
-      <Stack spacing={6}>
-        <Navigation />
-
-        {page}
-      </Stack>
-    </Page>
-  </>
+      {page}
+    </Stack>
+  </Page>
 );
